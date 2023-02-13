28f9a48b7e4d6aff75eeda35861e53bec395458a
Ashen Ishanka <ashen.ishanka74@gmail.com>
2023-02-13 16:59:54 +0530

    asd

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -109,13 +109,14 @@
                       <button class="btn btn-primary w-100" type="submit">Login</button>
                     </div>
                     <div class="col-12">
-                      <p class="small mb-0">Don't have account? <a href="{{url('/register')}}">Create an account</a></p>
+                      <p class="small mb-0">Don't have account? <a href="{{url('/signup')}}">Create an account</a></p>
                     </div>
                   </form>
 
                 </div>
               </div>
-
+              
+             
               <div class="credits">
                 Designed by <a href="https://facebook.com/">FuelIn</a>
               </div>
