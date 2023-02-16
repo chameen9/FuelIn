2572e2cce3aac52d1bacc270d987750674b26557
chameen9 <chameensandeepa9@gmail.com>
2023-02-16 11:37:02 +0530

    Cus Register

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,32 +1,223 @@
-<form method="post" action="{{ route('customers.store') }}">
-  @csrf
-  <div class="form-group">
-    <label for="first_name">First Name:</label>
-    <input type="text" class="form-control" id="first_name" name="first_name">
-  </div>
-  <div class="form-group">
-    <label for="last_name">Last Name:</label>
-    <input type="text" class="form-control" id="last_name" name="last_name">
-  </div>
-  <div class="form-group">
-    <label for="contact_number">Contact Number:</label>
-    <input type="text" class="form-control" id="contact_number" name="contact_number">
-  </div>
-  <div class="form-group">
-    <label for="national_identity_number">National Identity Number:</label>
-    <input type="text" class="form-control" id="national_identity_number" name="national_identity_number">
-  </div>
-  <div class="form-group">
-    <label for="address">Address:</label>
-    <input type="text" class="form-control" id="address" name="address">
-  </div>
-  <div class="form-group">
-    <label for="email">Email:</label>
-    <input type="email" class="form-control" id="email" name="email">
-  </div>
-  <div class="form-group">
-    <label for="password">Password:</label>
-    <input type="password" class="form-control" id="password" name="password">
-  </div>
-  <button type="submit" class="btn btn-primary">Sign Up</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>FuelIn | Register</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link rel = "icon" href = "{{URL::asset('/images/Xicon.ico')}}" type = "image/x-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="Assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="Assets/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="Assets/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="Assets/quill/quill.snow.css" rel="stylesheet">
+  <link href="Assets/quill/quill.bubble.css" rel="stylesheet">
+  <link href="Assets/remixicon/remixicon.css" rel="stylesheet">
+  <link href="Assets/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="/css/style.css" rel="stylesheet">
+ 
+
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="{{url('/login')}}" class="logo d-flex align-items-center w-auto">
+                  <img src="{{URL::asset('/images/Logo.png')}}" alt="FeulIn" style="width: auto; height: auto;">
+                  <span>FuelIn</span>
+                </a>
+              </div>
+              <!-- End Logo -->
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+                    <p class="text-center small">Enter your personal details to create account</p>
+                  </div>
+
+                  <form class="row g-3 needs-validation" method="post" action="{{ route('customers.store') }}">
+                    @csrf
+                    <div class="row">
+                      <div class="col-6">
+                        <label for="yourName" class="form-label">First Name:</label>
+                        <input type="text"class="form-control" id="first_name" name="first_name" required>
+                        <div class="invalid-feedback">Please, enter your first name!</div>
+                      </div>
+
+                      <div class="col-6">
+                        <label for="yourName" class="form-label">Last Name:</label>
+                        <input type="text"class="form-control" id="last_name" name="last_name" required>
+                        <div class="invalid-feedback">Please, enter your last name!</div>
+                      </div>
+                    </div>
+                    
+
+                    <div class="col-12">
+                      <label for="yourEmail" class="form-label">Your Email</label>
+                      <input type="email" name="email" class="form-control" id="yourEmail" required>
+                      <div class="invalid-feedback">Please enter a valid Email adddress!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="contact_number" class="form-label">Contact Number:</label>
+                      <input type="tel" class="form-control" id="contact_number" name="contact_number" required>
+                      <div class="invalid-feedback">Please enter a Contact Number.</div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="national_identity_number" class="form-label">National Identity Number:</label>
+                      <input type="text" class="form-control" id="national_identity_number" name="national_identity_number" required>
+                      <div class="invalid-feedback">Please enter your NIC!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="address" class="form-label">Address:</label>
+                      <input type="text" class="form-control" id="address" name="address" required>
+                      <div class="invalid-feedback">Please enter your NIC!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="password" class="form-label">Password</label>
+                      <input type="password" name="password" class="form-control" id="password" required>
+                      <div class="invalid-feedback">Please enter your password!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Already have an account? <a href="/login">Log in</a></p>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                Designed by <a href="https://facebook.com/">FuelIn</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        
+      </section>
+
+    </div>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  @if ($message = Session::get('success'))
+    <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="successModalLabel">Success</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-success">
+                        {{ $message }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function() {
+          $('#successModal').modal('show');
+          setTimeout(function() {
+            $('#successModal').modal('hide');
+          }, 2000);
+        });
+    </script> 
+  @endif
+
+  @if ($msg = Session::get('error'))
+    <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="errorModalLabel">Error</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-danger">
+                        {{ $msg }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function() {
+          $('#errorModal').modal('show');
+          setTimeout(function() {
+            $('#errorModal').modal('hide');
+          }, 2000);
+        });
+    </script>          
+  @endif
+
+  @if (count($errors)>0)
+    <div class="modal fade" id="valModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="errorModalLabel">Error</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            <a href="#"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></a></button>
+                        @endforeach
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function() {
+          $('#valModal').modal('show');
+          setTimeout(function() {
+            $('#valModal').modal('hide');
+          }, 5000);
+        });
+    </script> 
+  @endif 
+  <!-- Latest compiled JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
